febcd7db6771a865a19659917bf10250b7091dde
AndreyTomi <andrey.tomi@ukr.net>
2023-06-12 08:01:47 +0300

    fd

---
+++ src/lesson2.java
@@ -0,0 +1,27 @@
+public class lesson2 {
+    public static void main(String[] args) {
+        // типи данних для цілих числе
+        // -128 до 127 1 byte
+        byte byteWow = 22;
+        // -32.768 do 32.767 2 bytes
+        short shortWow = 123;
+        // -2.147.648 do 2.147.647 4 bytes
+        int intWow = 457;
+        // -9,223,372,036,854,775,808 do 9,223,372,036,854,775,807 8 bytes
+        long longWow = 10722908;
+        // типи даних  для не цілих чисел
+        // -3.4 * 1038 do 3.4 * 1038 в float обов'язково вказувати в кіінці числа f 4 bytes
+        float floatWow = 10.3f;
+        //8bytes
+        double doubleWow = 1234.5;
+        // типи данних для символів
+        //2 bytes
+        char charWow = '*';
+        //1 bytes true або false
+        boolean booleanWow = true;
+        //
+        System.out.println(booleanWow);
+    }
+
+
+}
